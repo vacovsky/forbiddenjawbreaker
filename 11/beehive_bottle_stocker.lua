4f5ab71385cfd04ea1693efef20592a395c0a59e
Joe Vacovsky <vacovsky@gmail.com>
2025-02-15 01:34:20 -0800

    tweaks

--- 11/beehive_bottle_stocker.lua
+++ 11/beehive_bottle_stocker.lua
@@ -7,14 +7,14 @@ local sc = require "lib/sc"
 function StockBottles()
     for _, hive in pairs(net.ListMatchingDevices(vars.hives)) do
         -- local bottles_replenished = whi.GetFromAnyWarehouse(false, 'minecraft:glass_bottle', hive, 4)
-        local bottles_replenished = sc.pull('minecraft:glass_bottle', 8, true, hive, 4)
+        local bottles_replenished = sc.pull('minecraft:glass_bottle', 1, true, hive, 4)
+        print(bottles_replenished, hive)
         if bottles_replenished > 0 then print('Stocked', bottles_replenished, '->', hive) end
     end
 end
 
 while true do
-    -- StockBottles()
-    if not pcall(StockBottles) then print('StockBottles() failed to complete') end
-
+    StockBottles()
+    -- if not pcall(StockBottles) then print('StockBottles() failed to complete') end
     sleep(1)
 end
